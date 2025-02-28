0c41a4cfe27e35527c0f85d8b1e357ebe1f520e6
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-02-28 09:38:47 -0500

    solucion punto 4

---
+++ Fundamentos del Lenguaje/solucion4.py
@@ -0,0 +1,8 @@
+# Recibir una frase del usuario
+frase = input("Introduce una frase: ")
+
+# Contar las palabras en la frase
+numero_palabras = len(frase.split())
+
+# Mostrar el número de palabras
+print(f"El número de palabras en la frase es: {numero_palabras}")
