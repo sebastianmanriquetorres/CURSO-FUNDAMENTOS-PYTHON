faac7d296b4e07278305e7624eaf2d11c2aa95e5
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-04-07 08:34:34 -0500

    Delete Fundamentos del Lenguaje/solucion2.1.py

--- Fundamentos del Lenguaje/solucion2.1.py
+++
@@ -1,6 +0,0 @@
-temperatura = int(input("ingrse la temperatura del motor"))
-if temperatura >=80:
-    print("el motor exede la temperatura, por ende será apagado")
-else:
-    print("el motor no exede la temperatura, por ende seguirá encendido")
-print("fin del programa")
