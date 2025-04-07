d9602a29f485e5a16849412979d8273a12cc5a4b
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-04-07 08:37:07 -0500

    Delete Fundamentos del Lenguaje/solucion4.py

--- Fundamentos del Lenguaje/solucion4.py
+++
@@ -1,8 +0,0 @@
-# Recibir una frase del usuario
-frase = input("Introduce una frase: ")
-
-# Contar las palabras en la frase
-numero_palabras = len(frase.split())
-
-# Mostrar el número de palabras
-print(f"El número de palabras en la frase es: {numero_palabras}")
