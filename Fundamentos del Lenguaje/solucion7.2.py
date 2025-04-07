ed596cbc8332f3bbfa58dde0a70f7e552e16317b
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-04-07 08:39:30 -0500

    Delete Fundamentos del Lenguaje/solucion7.2.py

--- Fundamentos del Lenguaje/solucion7.2.py
+++
@@ -1,6 +0,0 @@
-import re
-frase =("ingrese una frase: ")
-palabras = frase.split()
-palabras_limpias = tuple(sorted(re.sub(r'[^a-zA-Z]', '', palabra) for palabra in palabras))
-
-print("palabras procesadas y ordenadas:", palabras_limpias)
